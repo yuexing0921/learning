88f7e6ed60a6febb23e850559afc01356070d978
易全文 <yiquanwen@ezbuy.com>
2018-04-08 18:42:05 +0800

    增加格式化代码配置

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: "airbnb",
+    rules: {
+        indent: ["error", "tab"],
+        eqeqeq: "off",
+        "no-use-before-define": "warn",
+        "prefer-const": "warn",
+        "prefer-rest-params": "warn",
+        "no-prototype-builtins": "warn",
+        "consistent-return": "off",
+        "no-continue": "off",
+        "no-useless-escape": "off",
+        "no-plusplus": "off",
+        "array-callback-return": "off"
+    }
+};
